f630cf7e9d979ae3a644e1de3b689c9bea880141
Jasdeep <f20170748@goa.bits-pilani.ac.in>
2021-09-17 05:11:32 +0530

    To do List

---
+++ Project8-ToDoList_JS/main.js
@@ -0,0 +1,24 @@
+let btn=document.getElementById('bttn');
+
+let output=document.getElementById('output');
+
+let input=document.getElementById('input');
+
+btn.addEventListener('click',function(){
+    var paragraph=document.createElement('p');
+    paragraph.classList.add('paragraph-style');
+    paragraph.innerText=input.value;
+    output.appendChild(paragraph);
+    input.value="";
+
+    paragraph.addEventListener('click',function(){
+        paragraph.style.textDecoration="line-through";
+    });
+
+    paragraph.addEventListener('dblclick',function(){
+        output.removeChild(paragraph);
+    });
+
+});
+
+
